c06a053c692e0fdac6fe17c631d96efe1b28feaf
kinyipchan913 <100573103+kinyipchan913@users.noreply.github.com>
2024-01-03 14:11:34 +0000

    added median blur

--- opencv_tesserect.py
+++ opencv_tesserect.py
@@ -3,11 +3,12 @@ import cv2
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-img = cv2.imread('DSC_7205.JPG')
+img = cv2.imread('DSC_7213.JPG')
+img = cv2.medianBlur(img,3)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img_red = img[:,:,2]
-img_green = img[:,:,1]
-img_blue = img[:,:,0]
+# img_red = img[:,:,2]
+# img_green = img[:,:,1]
+# img_blue = img[:,:,0]
 
 equ = cv2.equalizeHist(img_gray)
 # ret, img_thre = cv2.threshold(equ, 50, 200, cv2.THRESH_BINARY)
