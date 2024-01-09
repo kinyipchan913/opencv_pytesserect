ff8eca351298668cf6f4daf44971fe843ddc0964
kinyipchan913 <kinyipchan913@gmail.com>
2024-01-09 14:12:53 +0000

    added readme and requirments

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # opencv_pytesserect
+
+
+##  Install tesseract
+https://github.com/UB-Mannheim/tesseract/wiki
+
+## Python requirements
+Python requirements for building pyPhase can be install using the following command:
+```
+pip install -r requirements.txt
+```
--- opencv_tesserect.py
+++ opencv_tesserect.py
@@ -3,7 +3,7 @@ import cv2
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-img = cv2.imread('DSC_7213.JPG')
+img = cv2.imread('uk_dronetheoreytest_result.png')
 img = cv2.medianBlur(img,3)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # img_red = img[:,:,2]
@@ -15,7 +15,9 @@ equ = cv2.equalizeHist(img_gray)
 img_thre = cv2.adaptiveThreshold(equ,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
             cv2.THRESH_BINARY,35, 35)
 text = pytesseract.image_to_string(img_thre)
-print(text)
+with open('file.txt', mode = 'w') as f:
+    f.write(text)
+    print(text)
 
 img_show = cv2.resize(img_thre, (0,0), fx=0.2, fy=0.2)
 
